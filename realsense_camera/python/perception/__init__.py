fb5bfcccb2607bc89ed85273ebed1336ecaec5b4
Shubham Singh <shubham.singh@edu.rptu.de>
2026-06-16 16:04:14 +0200

    aor updated with report

--- realsense_camera/python/perception/__init__.py
+++ realsense_camera/python/perception/__init__.py
@@ -1,1 +1,4 @@
 from .red_black_roi import detect_red_black_in_roi
+from .yolo_detector import YoloDetector
+from .depth_estimation import median_depth_in_box, attach_distances
+from .hazard import HazardObject, HazardRepresentation
--- realsense_camera/python/risk/__init__.py
+++ realsense_camera/python/risk/__init__.py
@@ -5,3 +5,6 @@ from .risk_score import (
 	risk_label,
 )
 from .runtime import RiskRuntimeState
+from .category_weights import category_weight, CATEGORY_WEIGHTS, LIVING, DYNAMIC, STATIC
+from .distance_risk import distance_risk_g, RMAX, D_MIN
+from .runtime_risk_model import object_risk, compute_object_risks, environmental_risk
